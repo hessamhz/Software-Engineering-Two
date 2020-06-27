f1cc2bee67afd548f356c5dc9c6a03e130e0ce0b
Hessam <hessam.hashemi21@gmail.com>
2020-06-27 13:30:34 +0430

    Directory Fix

--- backend/category/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from . model import Category
-# Register your models here.
-class CategoryAdmin(admin.ModelAdmin):
-    model = Category
-
-admin.site.register(Category, CategoryAdmin)

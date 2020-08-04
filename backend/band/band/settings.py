6f3fd08471dafed3667be59ebdbf1cca84286b65
Hessam <hessam.hashemi21@gmail.com>
2020-08-04 05:10:14 +0430

    static change

--- backend/band/band/settings.py
+++ backend/band/band/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'itbq-03^*0ajb1fabirk9l-)_$rynfa^(39)#n^tx&1k)d^&0+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.195.36.182', 'localhost']
+ALLOWED_HOSTS = ['51.195.36.182', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',

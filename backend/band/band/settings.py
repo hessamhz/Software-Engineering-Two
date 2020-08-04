c2e90eadf6b6577ec84c3254ddd0707871676fc5
Hessam <hessam.hashemi21@gmail.com>
2020-08-04 05:16:45 +0430

    added host

--- backend/band/band/settings.py
+++ backend/band/band/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'itbq-03^*0ajb1fabirk9l-)_$rynfa^(39)#n^tx&1k)d^&0+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.195.36.182', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['51.195.36.182', 'localhost', '127.0.0.1', 'dar.band']
 
 
 # Application definition

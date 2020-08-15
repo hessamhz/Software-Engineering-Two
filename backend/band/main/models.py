55e5da1466e08dcc649ae49f4428d0d64a257fc8
NegarES <n.estaki@yahoo.com>
2020-08-15 11:46:48 +0430

    chart
    add chart for price profile

--- backend/band/main/models.py
+++ backend/band/main/models.py
@@ -5,7 +5,7 @@ class PriceProfile(models.Model):
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=128, null=True, blank=True)
     percent = models.IntegerField(default=0)
-
+    created_date = models.DateTimeField()
     def __str__(self):
         return self.title
 

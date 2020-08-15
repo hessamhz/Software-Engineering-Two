f0764989cd18d9a2086b2ce20c320d7dcf3f55bc
fatemehkarimi <fatemehkarimi.1998@yahoo.com>
2020-08-15 23:57:16 +0430

    add page for order tracking

--- backend/band/main/urls.py
+++ backend/band/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import HomePageView, ProductByCategoryListView
+from .views import HomePageView, ProductByCategoryListView, OrderTrackingView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('order/track', OrderTrackingView.as_view(), name='track'),
     # path('by/category', ProductByCategoryListView.as_view(), name='product_cat'),
 ]
--- backend/band/main/views.py
+++ backend/band/main/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView, ListView, View
 
 from product.models import Product
 from category.models import Category
+from .models import Order
 # Create your views here.
 
 
@@ -28,3 +29,23 @@ class ProductByCategoryListView(ListView):
             if len(pr):
                 related_products[c.name] = pr
         return render(request, self.template_name, {'related_products': related_products})
+
+
+class OrderTrackingView(TemplateView):
+    template_name = 'track_order.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['track_code_valid'] = 0
+        tracking_code = self.request.GET.get('track_code')
+        if not tracking_code: return context
+
+        try:
+            tracking_code = int(tracking_code)
+            o = Order.objects.get(pk=tracking_code)
+            context['track_code_valid' ] = 1
+        except Exception as e:
+            context['track_code_valid' ] = 2
+            print("Error:", e, tracking_code)
+
+        return context

3f73b32f9a1334228fa653b0ad3b28474fd543a9
NegarES <n.estaki@yahoo.com>
2020-08-15 11:43:12 +0430

    chart
    add chart for category

--- backend/band/category/admin.py
+++ backend/band/category/admin.py
@@ -1,6 +1,45 @@
+import json
+
 from django.contrib import admin
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Count
+from django.db.models.functions import TruncDay
+from django.http import JsonResponse
+from django.urls import path
 
 from .models import Category
-# Register your models here.
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_date")
+    ordering = ("-created_date",)
+
+    # Inject chart data on page load in the ChangeList view
+    def changelist_view(self, request, extra_context=None):
+        chart_data = self.chart_data()
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data": as_json}
+        return super().changelist_view(request, extra_context=extra_context)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        extra_urls = [
+            path("chart_data/", self.admin_site.admin_view(self.chart_data_endpoint))
+        ]
+        # NOTE! Our custom urls have to go before the default urls, because they
+        # default ones match anything.
+        return extra_urls + urls
+
+    # JSON endpoint for generating chart data that is used for dynamic loading
+    # via JS.
+    def chart_data_endpoint(self, request):
+        chart_data = self.chart_data()
+        return JsonResponse(list(chart_data), safe=False)
+
+    def chart_data(self):
+        return (
+            Category.objects.annotate(date=TruncDay("created_date"))
+            .values("date")
+            .annotate(y=Count("id"))
+            .order_by("-date")
+        )
--- backend/band/category/models.py
+++ backend/band/category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-
+    created_date = models.DateTimeField()
     class Meta:
         verbose_name_plural = 'Categories'
 
